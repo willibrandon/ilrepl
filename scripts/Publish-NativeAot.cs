37f9ea67db6d4c57cad71f06e1a2659f3d7abdd3
Brandon Williams <willibrandon@gmail.com>
2026-09-18 21:46:39 -0700

    Bump package version to 0.5.2

--- scripts/Publish-NativeAot.cs
+++ scripts/Publish-NativeAot.cs
@@ -16,7 +16,7 @@ var ridOption = new Option<string>("--rid") { Description = "The runtime identif
 var versionOption = new Option<string>("--package-version")
 {
     Description = "The package version, with or without a leading v.",
-    DefaultValueFactory = _ => "0.5.1",
+    DefaultValueFactory = _ => "0.5.2",
 };
 var outputOption = new Option<string>("--output")
 {
